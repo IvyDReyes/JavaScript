c8f6144c206a33e8cd7d0e8ef93c81cf815c5e02
IVY D <71013378+IvyDReyes@users.noreply.github.com>
2025-03-08 00:01:40 +1300

    add examples of while loops and loop control in JavaScript

---
+++ Loops/Repeating_Code_with_While_Loops/While_Loops/while.js
@@ -0,0 +1,8 @@
+while (false) {
+    console.log("Skip me!");
+}
+
+while (true) {
+    console.log("To infinity,");
+    console.log("and Beyond!");
+}
---
+++ Loops/Stopping_While_Loops/Break_While_Loops/keepgoing.js
@@ -0,0 +1,6 @@
+let keepGoing = true;
+while (keepGoing === true) {
+    console.log("keepGoing: " + keepGoing);
+    keepGoing = false;
+    console.log("keepGoing: " + keepGoing);
+}
