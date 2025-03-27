c3c139d3e8b0e44cc2166139a5610f7355bdffa8
IVY D <71013378+IvyDReyes@users.noreply.github.com>
2025-03-27 23:27:48 +1300

    add examples of dynamic attribute changes and flexbox styling in HTML and JavaScript

--- Dynamic_Webpages/Setting_Attributes_Dynamically/Attribute_Changing/attribute.js
+++ Dynamic_Webpages/Setting_Attributes_Dynamically/Attribute_Changing/attribute.js
@@ -1,6 +1,47 @@
+/*
+
+//image
 function showAttribute() {
     const el = document.querySelector("img");
     const paragraph = document.querySelector("p");
     paragraph.innerHTML = el.src;
 }
 
+
+//href
+function showAttribute() {
+    const el = document.querySelector("a");
+    const paragraph = document.querySelector("p");
+    paragraph.innerHTML = el.href;
+}
+
+
+
+//update image
+function changeAttribute() {
+    const el = document.querySelector("img"); 
+    el.src = "./tolits.jpg"; 
+}
+
+
+
+
+//use attribute to change elements work and behave on a webpage
+function changeQuestionType() {
+    const el = document.querySelector("input");
+    el.type = "checkbox";
+}
+
+
+
+function addStyleSheet() {
+    const el = document.querySelector("link");
+    el.href = "style.css";
+}
+
+*/
+
+function addFlexbox() {
+    const el = document.querySelector("link");
+    el.href = "style.css";
+}
